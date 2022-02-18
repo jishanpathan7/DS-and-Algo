07e4bd5963b44465f28ed55f7db37ea050f004b4
jishan <jishanpatel78@gmail.com>
2022-02-18 18:34:37 +0530

    commit

---
+++ Leetcode/DSA/index.js
@@ -0,0 +1,41 @@
+
+// function isPalindrome(str) {
+//   var len = str.length;
+//   for (var i = 0; i < len / 2; i++) {
+//     if (str[i] !== str[len - i - 1]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// console.log(isPalindrome('madam'));
+
+//time complexity of this algorithm is O(n)
+//space complexity of this algorithm is O(1)
+
+
+function makePalindrome(str) { //O(n)
+  let len = str.length; //
+  if(len ===1) // if length is 1 then it is palindrome
+  return true; 
+  let i = 0;
+ let j = len-1;
+ while(j >= i){ //we are comparing the first and last element of the string
+   if(str[i] !== str[j]){ // if they are not equal then we are returning false
+       return false; // and breaking the loop
+    i++; // if they are equal then we are incrementing i and j
+    j--; 
+   }
+   return true; // if they are equal then we are returning true
+ }
+}
+
+function noAppend(s) { // function to check if the string is palindrome or not
+    if(makePalindrome(s.split(""))){  //we are splitting the string and passing it to makePalindrome function for checking if it is palindrome or not
+        return 0; // if it is palindrome then we are returning 0
+    }
+    s = s.substring(1); // if it is not palindrome then we are removing the first element of the string and passing it to the function again
+    return 1+noAppend(s); // and returning the value of the function
+}
+
+console.log(noAppend("wetgorm"));
---
+++ Leetcode/JsPractice.js
@@ -0,0 +1,50 @@
+// Himanshu the CR of class wants to spread some news about an event to every group of students of the college.
+// He will have to miss Professor DK classes to spread the news to every student, 
+// so he came up with a plan,Instead of meeting every student in person he decided to meet few people such that the person to whom he pass the news will spread it to all his friends in a group.
+// And those friends will pass it to other friends and so on.
+// Your task is to find minimum number of persons he need to meet to spread the news of the event.
+// Input:
+// First line of input contains number of test cases T.
+// First line of each test case contains number of students N.
+//The next line will contain 'e', the number of friend descriptions 'e'.
+//The next 'e' lines are the descriptions of type "a b" denoting 'a' friends with 'b'. If 'a' is friends with 'b' then 'b' is friends with 'a'.
+//Output:
+//Output contains T lines, the number of persons he need to meet to spread the news of the event.
+//Constraints:
+//1<=T<=10
+//2<=N<=10^4
+//0<=e<=N/2
+//0<=a,b<=N-1
+//Example:
+//Input:
+//2
+//4
+//2
+//0 1
+//1 2
+//3
+//0 
+
+//Output:
+//2
+//3
+
+function spreadNews(n, e, arr) {
+    let friends = new Array(n).fill(0);
+    let count = 0;
+    for (let i = 0; i < e; i++) {
+        let [a, b] = arr[i].split(' ').map(Number);
+        friends[a]++;
+        friends[b]++;
+    }
+    for (let i = 0; i < n; i++) {
+        if (friends[i] == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(spreadNews(4, 2, ['0 1', '1 2']));
+
+
+
